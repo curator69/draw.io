170d05b8c40af5f47551e6380de1e7261f2ab828
curator69 <75662004+curator69@users.noreply.github.com>
2022-10-22 03:28:00 +0530

    completed status part

--- github_issues/src/App.js
+++ github_issues/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div>
       <main>
+        <h1 className="heading">Enter the repo link:</h1>
         <input className="input-tab" placeholder="repo link"></input>
         <StatusResults />
         {/* <WeekResults /> */}
--- github_issues/src/components/StatusResults.jsx
+++ github_issues/src/components/StatusResults.jsx
@@ -7,20 +7,18 @@ const StatusResults = () => {
   const octokit = new Octokit({ auth: process.env.REACT_APP_GH_PAT });
 
   const fetchData = async () => {
-    for (let i = 0; i < 1; i++) {
-      const data = await octokit
-        .request("GET /repos/{owner}/{repo}/issues", {
-          owner: "mui",
-          repo: "material-ui",
-          state: "closed",
-          page: 2,
-          per_page: 25,
-        })
-        .then((response) => response.data)
-        .then((data) => data.map((issue) => issue));
-      console.log(data);
-      setIssues(data);
-    }
+    const data = await octokit
+      .request("GET /repos/{owner}/{repo}/issues", {
+        owner: "axios",
+        repo: "axios",
+        state: "all",
+        page: 2,
+        per_page: 25,
+      })
+      .then((response) => response.data)
+      .then((data) => data.map((issue) => issue));
+    console.log(data);
+    setIssues(data);
   };
 
   return (
@@ -29,11 +27,15 @@ const StatusResults = () => {
       <button onClick={() => fetchData()}>fetch</button>
       <div>
         <h2>Open Issues Count</h2>
-        <ol>
+        <ol className="issues-list">
           {issues.map((issue) => (
-            <li key={issue.id}>
-              {issue.title}{" "}
-              <span className={(issue.state = "closed" && "open")}>
+            <li className="issue" key={issue.id}>
+              {issue.title} {""}
+              <span
+                className={
+                  (issue.state = "open" && issue.closed_at ? "open" : "closed")
+                }
+              >
                 {issue.state}
               </span>
             </li>
